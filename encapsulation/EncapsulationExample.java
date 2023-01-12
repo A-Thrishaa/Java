67a2ce260440aa3cfed44939d18d49a98246091c
A-Thrishaa <122152990+A-Thrishaa@users.noreply.github.com>
2023-01-12 09:53:39 +0530

    Add files via upload

---
+++ encapsulation/EncapsulationExample.java
@@ -0,0 +1,12 @@
+//encapsulation example
+package encapsulation;
+import java.util.*; //importing built-in package
+class EncapsulationExample//main class
+{  
+public static void main(String[] args)
+{  
+Students s=new Students();  //creating object for encapsulated class
+s.setNumber(31);  //setting number
+System.out.println(s.getNumber());  //printing number
+}  
+}
---
+++ encapsulation/Students.java
@@ -0,0 +1,16 @@
+//encapsulation example
+package encapsulation;
+import java.util.*; //importing built-in package
+public class Students//encapsulated class
+{  
+private int num;    //private data member
+public int getNumber()//method to set number
+{  
+return num;  
+}    
+public void setNumber(int num)//method to print number
+{  
+this.num=num; 
+}  
+}  
+
